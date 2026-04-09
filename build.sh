8453b94b5224c99930ed51b2372decf8d9330755
Vaishnavitripathi1003 <119799306+Vaishnavitripathi1003@users.noreply.github.com>
2026-04-09 14:36:18 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 
-# Install Flutter
-git clone https://github.com/flutter/flutter.git --depth 1
+# Clone Flutter (specific stable version)
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable
+
+# Set path
 export PATH="$PATH:`pwd`/flutter/bin"
 
+# Verify Flutter (IMPORTANT)
+flutter doctor
+
+# Enable web
 flutter config --enable-web
+
+# Clean old builds
+flutter clean
+
+# Get dependencies
 flutter pub get
+
+# Build web
 flutter build web
